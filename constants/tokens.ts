81d258d87a875d811bbc5c1dfd46aa2540ffba84
Activx-nV <laurikamm@gmail.com>
2026-02-22 00:33:30 +0200

    create token constants

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -1,30 +1,4 @@
 export const TOKENS = {
-  1: [
-    {
-      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      symbol: "USDC",
-      name: "USD Coin",
-      decimals: 6,
-      logo: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png",
-      coingeckoId: "usd-coin",
-    },
-    {
-      address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      symbol: "DAI",
-      name: "Dai Stablecoin",
-      decimals: 18,
-      logo: "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png",
-      coingeckoId: "dai",
-    },
-    {
-      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-      symbol: "USDT",
-      name: "Tether USD",
-      decimals: 6,
-      logo: "https://cryptologos.cc/logos/tether-usdt-logo.png",
-      coingeckoId: "tether",
-    },
-  ],
   11155111: [
     {
       address: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
@@ -35,12 +9,46 @@ export const TOKENS = {
       coingeckoId: "usd-coin",
     },
     {
+      address: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+      symbol: "LINK",
+      name: "Chainlink Token",
+      decimals: 18,
+      logo: "https://cryptologos.cc/logos/chainlink-link-logo.png",
+      coingeckoId: "chainlink",
+    },
+    {
+      address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+      symbol: "UNI",
+      name: "Uniswap",
+      decimals: 18,
+      logo: "https://cryptologos.cc/logos/uniswap-uni-logo.png",
+      coingeckoId: "uniswap",
+    },
+  ],
+  421614: [
+    {
       address: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
       symbol: "USDC",
       name: "USD Coin (Arbitrum Sepolia)",
       decimals: 6,
       logo: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png",
       coingeckoId: "usd-coin",
+    },
+    {
+      address: "0xb1D4538B4571d411F07960EF2838Ce337FE1E80E",
+      symbol: "LINK",
+      name: "Chainlink Token",
+      decimals: 18,
+      logo: "https://cryptologos.cc/logos/chainlink-link-logo.png",
+      coingeckoId: "chainlink",
+    },
+    {
+      address: "0x4064DcC3A9DE3e8b8DE6C080740f1dEa7b1afF63",
+      symbol: "UNI",
+      name: "Uniswap",
+      decimals: 18,
+      logo: "https://cryptologos.cc/logos/uniswap-uni-logo.png",
+      coingeckoId: "uniswap",
     },
   ],
 } as const;
