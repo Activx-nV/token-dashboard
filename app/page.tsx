125c88a3cbe71b44b439645438769a4e4c4ff417
Activx-nV <laurikamm@gmail.com>
2026-02-22 00:26:11 +0200

    init project

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { TokenDashboard } from "@/components/TokenDashboard";
+import { TokenDashboard } from "@/components/dashboard/TokenDashboard";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8 max-w-7xl">
         <header className="flex flex-col md:flex-row justify-between items-center mb-12 gap-4">
           <div>
-            <h1 className="text-4xl font-bold text-white">ERC20 Tokens</h1>
+            <h1 className="text-4xl font-bold text-white">ERC-20</h1>
           </div>
           <ConnectButton />
         </header>
