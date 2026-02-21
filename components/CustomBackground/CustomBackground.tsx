20ef5c86fc3545437899ae7d1707755b7dc7d1ed
Activx-nV <laurikamm@gmail.com>
2026-02-22 00:30:49 +0200

    set custom background (react bits)

--- components/CustomBackground/CustomBackground.tsx
+++ components/CustomBackground/CustomBackground.tsx
@@ -1,11 +1,10 @@
-'use client';
-
-import LightPillar from './LightPillar';
+"use client";
+import Threads from "./Threads";
 
 export const CustomBackground = () => {
   return (
     <div className="fixed inset-0 -z-10 pointer-events-none">
-      <LightPillar />
+      <Threads amplitude={1.5} distance={0} enableMouseInteraction={false} />
     </div>
   );
 };
